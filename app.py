cea61dc68292958a7eae48629537679390efb632
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-08-26 23:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import re
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def image_viewer():
-    name = "gojo"
+    if request.method == 'POST':
+        name = request.form.get('search_query')
+        if not name:
+            return render_template('index.html', message="Please enter a search query.")
+    else:
+        name = "gojo"
+
     source = f"https://www.bing.com/images/search?q={name}&form=HDRSC3&pc=EMMX01&first=1"
     data = str(requests.get(source).content)
     pattern = r'https:\/\/[^\s"&]+\.jpg'
